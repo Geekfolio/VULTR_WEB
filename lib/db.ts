8721e6a21d0726e17539abdc9de070285ba8b467
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-03-01 19:46:22 +0530

    fuck ansible + db pool configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,13 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.NEXT_DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
+  idleTimeoutMillis: 20000,
+  max: 10,
 });
 
 export default pool;
-  
+
